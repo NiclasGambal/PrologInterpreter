a8327b0742f15a40d822a8e1da9520747d4de5e5
ng217779 <raceread@gmail.com>
2022-03-01 18:12:33 +0100

    a bit more for four, I progress if 3 is finished

--- Substitution.hs
+++ Substitution.hs
@@ -4,32 +4,34 @@ import Type
 
 import PrettyPrinting
 
-data Subst = Subst[(VarName, Term)]
+data Subst = Subst[(Term, VarName)]
     deriving Show
 
 -- Returns the domain of the substitution
 domain :: Subst -> [VarName]
-domain x = undefined
+domain (Subst []) = []
+domain (Subst (p:ps)) = [snd p] ++ (domain (Subst ps))
 
 -- Creates an empty substitution
 empty :: Subst
-empty = undefined 
+empty = Subst []
 
 -- Creates an substitution which maps only one var on a term
-single :: VarName -> Term -> Subst
-single x y = undefined
+--single :: VarName -> Term -> Subst
+--single x y = undefined
 
 -- Applies an substitution on a term
-apply :: Subst -> Term -> Term
-apply s t = undefined 
+--apply :: Subst -> Term -> Term
+--apply s t = undefined 
 
 -- Composes two substitutions
-compose :: Subst -> Subst -> Subst
-compose x y = undefined
+--compose :: Subst -> Subst -> Subst
+--compose x y = undefined
 
 -- Restricts a substitution to a defined domain
-restrictTo :: Subst -> [VarName] -> Subst
-restrictTo x ys = undefined
+--restrictTo :: Subst -> [VarName] -> Subst
+--restrictTo x ys = undefined
 
+-- Instance of Subst of classtype Pretty
 instance Pretty Subst where
     pretty (Subst []) = "{}"
