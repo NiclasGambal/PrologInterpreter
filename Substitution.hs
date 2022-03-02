c851343dfe6d4d5277f9e0d0bf6d536b6948cc14
ng217779 <raceread@gmail.com>
2022-03-02 22:38:03 +0100

    id problem solved, will work on compose tomorrow

--- Substitution.hs
+++ Substitution.hs
@@ -1,32 +1,39 @@
-module Substitution where
+module Substitution (Subst, domain, empty, single, compose) where
 
 import Type
 
 import PrettyPrinting
 
-data Subst = Subst[(Term, VarName)]
+import Vars
+import Data.List
+
+data Subst = Subst[(VarName, Term)]
     deriving Show
 
 -- Returns the domain of the substitution
 domain :: Subst -> [VarName]
-domain (Subst []) = []
-domain (Subst (p:ps)) = [snd p] ++ (domain (Subst ps))
+-- Gets all first elements of the substitution. 
+domain (Subst ps) = nub (concatMap comparer ps)
+
+-- Checks if the vars of domain picture themself.
+comparer :: (VarName, Term) -> [VarName]
+comparer (v, t) = if v `elem` allVars t then [] else [v]
 
 -- Creates an empty substitution
 empty :: Subst
 empty = Subst []
 
 -- Creates an substitution which maps only one var on a term
---single :: VarName -> Term -> Subst
---single x y = undefined
+single :: VarName -> Term -> Subst
+single v t = Subst[(v,t)]
 
 -- Applies an substitution on a term
 --apply :: Subst -> Term -> Term
 --apply s t = undefined 
 
 -- Composes two substitutions
---compose :: Subst -> Subst -> Subst
---compose x y = undefined
+compose :: Subst -> Subst -> Subst
+compose s1 s2 = undefined 
 
 -- Restricts a substitution to a defined domain
 --restrictTo :: Subst -> [VarName] -> Subst
