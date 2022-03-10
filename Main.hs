6dbacfe8da272cd930b375412ed94ac8c495526a
ng217779 <raceread@gmail.com>
2022-03-10 05:03:45 +0100

    Main angefangen und SLD weitergemacht

---
+++ Main.hs
@@ -0,0 +1,72 @@
+module Main where
+
+import Type
+import SLD
+import Parser
+
+main :: IO ()
+main = do
+    putStr "Welcome to your personal Prolog Interpreter!\n\n"
+    putStr "Type \":h\" for help.\n"
+    repl_loop (Prog []) dfs
+
+repl_loop :: Prog -> Strategy -> IO ()
+repl_loop prog strat = do
+    putStr "?- "
+    str <- getLine
+    rep prog strat str
+
+rep :: Prog -> Strategy -> String -> IO ()
+rep prog strat (':':str) = eval prog strat str
+rep prog strat str = do
+    let x = parse str
+    case x of
+        (Left s) -> do
+            putStrLn s
+            repl_loop prog strat
+        (Right goal) -> do
+            returnResults (solveWith prog goal)
+            repl_loop prog strat
+
+returnResults :: [Subst] -> IO ()
+returnResults [] = do
+    putStr "false."
+    return ()
+returnResults (s:ss) = do
+    putStr ((pretty s) ++ " ")
+    cmd <- getLine 
+    parseInput cmd ss
+    return ()
+
+parseInput :: String -> [Subst] -> IO ()
+parseInput ('.':_) _ = do
+    return ()
+parseInput (';':_) [] = do
+    putStrLn "false."
+    return ()
+parseInput ";" subs = returnResults subs
+parseInput (';':xs) (s:ss) = do
+    putStrLn (pretty s)
+    parseInput xs ss
+parseInput _ _ = do
+    putStrLn "Error!"
+    return ()
+
+eval :: Prog -> Strategy -> String -> IO ()
+eval prog strat ('h':_) = do
+    putStrLn "<goal>      Solves/proves the specified goal."
+    putStrLn ":h          Shows this help message."
+    putStrLn ":l <file>   Loads the specified file."
+    putStrLn ":p          Prints the currently loaded program."
+    putStrLn ":q          Exits the interactive environment."
+    putStrLn ":s <strat>  Sets the specified search strategy"
+    putStrLn "            where <strat> is one of 'dfs' or 'bfs'."
+    repl_loop prog strat
+eval _ _ ('q':_) = do
+    putStrLn "Quit!"
+eval prog _ "s dfs" = do
+    putStrLn "dfs!"
+    repl_loop prog dfs
+eval prog _ "s bfs" = do
+    putStrLN "bfs!"
+    repl_loop prog bfs
--- SLD.hs
+++ SLD.hs
@@ -1,8 +1,10 @@
-module SLD where
+module SLD (sld, dfs, bfs, solveWith) where
 
 import Type
 
 import Substitution
+
+import Unifikation
 
 data SLDTree = Node Goal [(Subst, SLDTree)]
     deriving Show
@@ -10,13 +12,31 @@ data SLDTree = Node Goal [(Subst, SLDTree)]
 type Strategy = SLDTree -> [Subst]
 
 sld :: Prog -> Goal -> SLDTree
-sld p g = undefined 
+sld (Prog (r:rs)) (Goal (t:ts)) = Node (Goal (t:ts)) [let s = unifyGoals r  (Goal (t:ts)) in s, sld (Prog r:rs) applyOnAll s (Goal (t:ts))] ++ (next (Prog rs) (Goal t:ts))
+
+next :: Prog -> Goal -> [(Subst, SLDTree)]
+next (Prog []) _ = []
+next (Prog r:rs ) g = (let s = unifyGoals r  (Goal (t:ts)) in s,sld (Prog r:rs applyOnAll s g) ++ next (Prog rs) g
+
+applyOnAll :: Subst -> Goal -> Goal
+applyOnAll s (Goal ts) = Goal (map (apply s ) ts)
+
+unifyGoals :: Rule -> Goal -> Subst
+_ Goal [] = empty
+(Rule r rs) (Goal (t:ts)) =  compose (dontBeAMaybeSubst (unify r t) unifyGoals (Rule r rs) (Goal ts))
+
+
+-- Method to take the Substitution out of a Maybe Substitution
+dontBeAMaybeSubst :: Maybe Subst -> Subst
+dontBeAMaybeSubst Nothing    = empty
+dontBeAMaybeSubst (Just sub) = sub
 
 dfs :: Strategy
-dfs = undefined
+dfs (Node (Goal []) _) = [empty]
+dfs (Node _ []) = []
 
 bfs :: Strategy
 bfs = undefined 
 
 solveWith :: Prog -> Goal -> Strategy -> [Subst]
-solveWith p g strat = undefined
+solveWith p g strat = strat (sld p g)
