220ea549afe7d72a47210d8d52720ce9a3769b4c
ng217779 <raceread@gmail.com>
2022-03-11 00:53:16 +0100

    SLD funktion umstrukturiert

--- SLD.hs
+++ SLD.hs
@@ -8,6 +8,7 @@ import Substitution
 
 import Unifikation
 import PrettyPrinting
+import System.Win32 (GlobalAllocFlags)
 
 data SLDTree = Node Goal [(Subst, SLDTree)]
     deriving Show
@@ -31,7 +32,11 @@ sld (Prog rules) (Goal ts) = Node (Goal ts) (concatMap (\oneTerm -> (concatMap (
 --tryRules (r:rs) ts t = [(empty, (Node (Goal [t]) []))] ++ tryRules rs ts t
 
 tryRule:: Prog -> Goal -> Rule -> Term -> [(Subst, SLDTree)]
-tryRule p (Goal ts) (Rule rHead rTail) t = if ((unify rHead t) == Nothing) then [] else [(dontBeAMaybeSubst (unify rHead t), sld p (Goal ((filter (/= t) ts) ++ (map (apply (dontBeAMaybeSubst (unify rHead t))) rTail))))]
+tryRule p (Goal ts) (Rule rHead rTail) t = case unify rHead t of
+                                                Nothing -> []
+                                                Just s ->  [(s, sld p (Goal (map (apply s) (filter (/= t) ts ++ rTail))))]
+    
+--if ((unify rHead t) == Nothing) then [] else [(dontBeAMaybeSubst (unify rHead t), sld p (Goal ((filter (/= t) ts) ++ (map (apply (dontBeAMaybeSubst (unify rHead t))) rTail))))]
 --(sld p (Goal ((filter (/= t) ts) ++ (map (apply (dontBeAMaybeSubst (unify rHead t))) rTail))) ))]
 
 --tryRule :: Rule -> [Term] -> Term -> [(Subst, SLDTree)]
@@ -42,3 +47,15 @@ tryRule p (Goal ts) (Rule rHead rTail) t = if ((unify rHead t) == Nothing) then 
 dontBeAMaybeSubst :: Maybe Subst -> Subst
 dontBeAMaybeSubst Nothing    = empty
 dontBeAMaybeSubst (Just sub) = sub
+
+prog1 :: Prog 
+prog1 = Prog [(Rule (Comb "p" [(Var (VarName "X")),(Var (VarName "Z"))]) [(Comb "q" [(Var (VarName "X")),(Var (VarName "Y"))]),(Comb "p" [(Var (VarName "Y")),(Var (VarName "Z"))])]),(Rule (Comb "p" [(Var (VarName "X")),(Var (VarName "X"))]) []),(Rule (Comb "q" [(Comb "a" []),(Comb "b" [])]) [])]
+
+goal1 :: Goal
+goal1 = Goal [(Comb "p" [(Var (VarName "S")),(Comb "b" [])])]
+
+term1 :: Term
+term1 =  Comb "p" [((Comb "b" [])),(Comb "b" [])]
+
+rule1 :: Rule
+rule1 = (Rule (Comb "p" [(Var (VarName "X")),(Var (VarName "Z"))]) [(Comb "q" [(Var (VarName "X")),(Var (VarName "Y"))]),(Comb "p" [(Var (VarName "Y")),(Var (VarName "Z"))])])
